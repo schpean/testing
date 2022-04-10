9c0d207c30e03e2dcf22dbd8901e421e93d2c025
schpean <mateas.bogdan@gmail.com>
2022-04-10 10:37:45 -0700

    hj and jh

--- question4.sh
+++ question4.sh
@@ -10,8 +10,8 @@ read email
 git config --global user.name "$user"
 git config --global user.email "$email"
 
-source= "$pwd/.git/config"
-cp -r $source config
+source= "/home/schpean/Desktop/Question4/.git/config"
+cp -r /home/schpean/Desktop/Question4/.git/config /home/schpean/Desktop/Question4
 git add .
 git commit -m "$email and $user"
 git push --set-upstream origin main

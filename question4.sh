2f62fb6eb6c7b7253d04690366a12ee1673c64dd
Eu <lolol>
2022-04-10 08:38:53 -0700

    some test

---
+++ question4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#!/usr/bin/env bash
+
+echo -n "Please provide your name: "
+read name
+
+echo -n "Your email address: "
+read email
+git config --global user.name "$name"
+git config --global user.email "$email"
